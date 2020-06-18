f7ede686769bf0ca282c6880b2178a6964a17f3d
Ryan Ling <ryan@outlook.com.au>
2020-06-19 00:51:03 +1000

    Customise from template
    Splitting this out will be slightly more cumbersome to manage, but I've
    done this so that `seek-module-toolkit` can (hopefully) do a simple
    `extends: [skuba]` to align linting rules. It can't be smart with
    `require.resolve` as its config file is in YAML format.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const { testPathIgnorePatterns } = require('skuba/config/jest');
-
-module.exports = {
-  coveragePathIgnorePatterns: ['src/testing'],
-  preset: 'skuba',
-  setupFiles: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: [...testPathIgnorePatterns, '/test\\.ts'],
-};
