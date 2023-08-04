c8d4a8e28fdf81e4a1dcdaa89ab94028adc7c2fa
samchungy <samchungy@gmail.com>
2023-08-04 10:50:25 +1000

    upgrade

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ module.exports = {
   overrides: [
     {
       extends: [
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
       ],
       files: [`*.{${ts.join(',')}}`],
       parserOptions: {
